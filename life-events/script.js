098313fc5d51ec97bad7e5101662b9a68232ff66
Lex <105667221+Lex-G@users.noreply.github.com>
2022-06-27 19:32:34 -0700

    Add files via upload

---
+++ life-events/script.js
@@ -0,0 +1,26 @@
+const myName = "Lex";
+let myAge = 26;
+const myBirthday = "August 18th";
+let pinapplePizza = true;
+const lifeEvents = [
+  "I have one brother and five step-siblings",
+  "I have three dogs",
+  "I moved to Seattle from Tacoma in 2020",
+  "I got married February 24th, 2022",
+];
+if (pinapplePizza) {
+  console.log(
+    `My name is ${myName} and I like pineapples on pizza. I am currently ${myAge} years old ad my birthday is on ${myBirthday}.`
+  );
+} else {
+  console.log(
+    `My name is ${myName} and I'm not into pineapples on pizza. I am currently ${myAge} years old and my birthday is on ${myBirthday}`
+  );
+}
+
+// for (const i = 0; i <= lifeEvents.length; i++);
+// if (lifeEvents[i] !== "5") {
+//   console.log(lifeEvents[i]);
+// }
+Random randomGenerator = new Random();
+int randomNumber = random.randomGenerator.nextInt(10);
